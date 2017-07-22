c9205a73a685f06730df3d97f71aa3b1d77e6685
Sanledi Buli <sanledi.buli@gmail.com>
2017-07-22 21:43:34 +0700

    Added login screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { View, Text } from 'react-native';
 export default class App extends Component {
   render() {
     return(
-      <View>
+      <View style={styles.container}>
         <LoginScreen />
       </View>
     )
--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -1,14 +1,19 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import Wallpaper from '../components/Wallpaper';
+import Logo from '../components/Logo';
+import FormLogin from '../components/FormLogin';
+import SignupSection from '../components/SignupSection';
+import ButtonSubmit from '../components/ButtonSubmit';
 
 export default class LoginScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>
-          "Hello World"
-        </Text>
-      </View>
+      <Wallpaper>
+        <Logo />
+        <FormLogin />
+        <SignupSection />
+        <ButtonSubmit />
+      </Wallpaper>
     );
   }
 }
